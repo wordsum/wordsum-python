474368821aa29263ac412b8114a6e92970f88db4
Kalab Oster <kalaboster@gmail.com>
2017-09-06 21:51:00 -0700

    Edit to remove code no longer required to test this code thoughts as it is getting in the way in seeing beyond this while just beginning to realize what it does. I do have to make it personal for some reson. Publish because the script still does what I expect without the old commented-out code that I don't need to understand how this works.

--- wordsum/read/word2vec.py
+++ wordsum/read/word2vec.py
@@ -20,49 +20,12 @@ from __future__ import print_function
 
 import collections
 import math
-import os
 import random
-import zipfile
 
 import numpy as np
-import urllib
 import tensorflow as tf
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-
-'''
-Keep for now but delete soon.
-# Step 1: Download the data.
-url = 'http://mattmahoney.net/dc/'
-
-
-def maybe_download(filename, expected_bytes):
-    """Download a file if not present, and make sure it's the right size."""
-    if not os.path.exists(filename):
-        filename, _ = urllib.request.urlretrieve(url + filename, filename)
-    statinfo = os.stat(filename)
-    if statinfo.st_size == expected_bytes:
-        print('Found and verified', filename)
-    else:
-        print(statinfo.st_size)
-        raise Exception(
-            'Failed to verify ' + filename + '. Can you get to it with a browser?')
-    return filename
-
-
-filename = maybe_download('text8.zip', 31344016)
-
-
-# Read the data into a list of strings.
-def read_data(filename):
-    """Extract the first file enclosed in a zip file as a list of words."""
-    with zipfile.ZipFile(filename) as f:
-        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
-    return data
-'''
-# Default now I'll start local
-#filename = "text8"
-
 
 def build_dataset(words, n_words):
     """Process raw inputs into a dataset."""
