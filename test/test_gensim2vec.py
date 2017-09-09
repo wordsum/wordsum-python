0aa1074b887b76268e61636b28f874a003954ec6
Kalab Oster <kalaboster@gmail.com>
2017-09-09 09:18:01 -0700

    Write simple function to create a model from sentences using examples on the site and within the current source structure. It seems the best way to handle the model creation. And begin to write function to save the model in a path. Write a test just to make sure it works for now as I begin to integrate the test data for 0001.json and write the code to get narrator and no dialog from the wordsum text model and put it into the word2vec. Then test to see what the output means. Publish because something is working and it is enough to begin to connect the wordsum modeled text data into word2vec then into a story model.

--- test/test_gensim2vec.py
+++ test/test_gensim2vec.py
@@ -1,1 +1,17 @@
+'''
+Tests to test gensim.
+'''
+import wordsum.read.gensim2vec as gensim2vec
+
+
+def test_train_sentences():
+
+    sentences = [['A', 'beginning'], ['A', 'start']]
+
+    model = gensim2vec.train_sentences(sentences)
+
+    print(model.wv['A'])
+
+    assert  model!=None
+
 
--- wordsum/read/gensim2vec.py
+++ wordsum/read/gensim2vec.py
@@ -1,1 +1,38 @@
+import gensim
+import logging
+import os
+
+
+PATH_SCRIPT=os.path.dirname(os.path.realpath(__file__))
+PATH_TRAIN=os.path.join(PATH_SCRIPT, '../../data/train/')
+PATH_MODEL=os.path.join(PATH_SCRIPT, '../../data/model/')
+
+
+
+'''
+Train the model with lists of lists of sentences.
+
+TODO: either by object or parameters set remaining Word2Vec parameters and check.
+'''
+def train_sentences(sentences):
+    logging.debug("sentences: ", sentences)
+
+    if not sentences is None:
+        model = gensim.models.Word2Vec(sentences, min_count=1)
+    else:
+        print("No sentences")
+        model = None
+
+    return model
+
+
+'''
+save the model locally to later use.
+'''
+def save_model(model, path):
+
+    if not model is None:
+        model.save(path)
+    else:
+        print("No model to save.")
 
