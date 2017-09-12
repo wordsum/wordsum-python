425d74fe3767b82385deac92658726f4cbe42a9f
Kalab Oster <kalaboster@gmail.com>
2017-09-11 18:11:43 -0700

    Write and publish an outline of the code to write. Write the definition of the Python file. Publish to complete this idea and get it in a state of doing.

--- wordsum/read/pipelines/pipeline_plot_story.py
+++ wordsum/read/pipelines/pipeline_plot_story.py
@@ -1,1 +1,18 @@
+'''
+Pipeline to plot story per chapter then as an entire book and use the distance of the vector change per chaper plot through
+the story.
 
+'''
+# Get the wordsum model of the chapter and return the narrator strings.
+
+
+# ETL the strings here or before. I need to reason when to ETL. Should I make it a flag.
+
+
+# Input the data into word
+
+
+# Save the model locally
+
+
+# Save to container and push as an option.
