12e6e0ed192630bd1fbfded828d01435b5815276
Kalab Oster <kalaboster@gmail.com>
2017-09-22 08:00:30 -0700

    Rewrite from enum and Java POJO to this. This is how I will begin to rewrite how I get and define dialog for the fifth or sixth time, but this time leaving that old Java code behind that hurts the mind right now. This is near better, but once I start pushing state to a data store I will probably revisit and edit. For now, while I have not tests and haven't used. I am going to begin to use. Not sure if using object is the way. These hardcoded values. Publish to save state and time.

--- wordsum/read/models/dialog/dialog_mark.py
+++ wordsum/read/models/dialog/dialog_mark.py
@@ -1,40 +1,207 @@
+class Dialog_Mark:
+
+
+    def __init__(self):
+        self.telepathic_begin_unicode = "\u003e"
+        self.telepathic_continue_unicode = "\u003E\u007C"
+        self.telepathic_end_unicode = "\u003C"
+
+        self.internal_begin_unicode = "\u003C"
+        self.internal_continue_unicode = "\u003C\u007C"
+        self.internal_end_unicode = "\u003e"
+
+        self.audio_begin_unicode = "\u002C\u002C"
+        self.audio_continue_unicode = "\u0027\u0027"
+        self.audio_end_unicode = "\u0027\u0027"
+
+        self.telepathic_begin_character = ">"
+        self.telepathic_continue_character = ">|"
+        self.telepathic_end_character = "<"
+
+        self.internal_begin_character = "<"
+        self.internal_continue_character = "<|"
+        self.internal_end_character = ">"
+
+        self.audio_begin_character = ",,"
+        self.audio_continue_character = "''"
+        self.audio_end_character = "''"
+
+    '''
+    Telepathic Unicode
+    '''
+    @property
+    def telepathic_begin_unicode(self):
+        return self.telepathic_begin_unicode
+
+    @telepathic_begin_unicode.setter
+    def telepathic_begin_unicode(self, telepathic_begin_unicode):
+        self.telepathic_begin_unicode = telepathic_begin_unicode
+
+
+    @property
+    def telepathic_continue_unicode(self):
+        return self.telepathic_continue_unicode
+
+    @telepathic_continue_unicode.setter
+    def telepathic_continue_unicode(self, telepathic_continue_unicode):
+        self.telepathic_continue_unicode = telepathic_continue_unicode
+
+
+    @property
+    def telepathic_end_unicode(self):
+        return self.telepathic_end_unicode
+
+    @telepathic_end_unicode.setter
+    def telepathic_end_unicode(self, telepathic_end_unicode):
+        self.telepathic_end_unicode = telepathic_end_unicode
+
+
+    '''
+    Internal Unicode
+    '''
+    @property
+    def internal_begin_unicode(self):
+        return self.internal_begin_unicode
+
+    @internal_begin_unicode.setter
+    def internal_begin_unicode(self, internal_begin_unicode):
+        self.internal_begin_unicode = internal_begin_unicode
+
+
+    @property
+    def internal_continue_unicode(self):
+        return self.internal_continue_unicode
+
+    @internal_continue_unicode.setter
+    def internal_continue_unicode(self, internal_continue_unicode):
+        self.internal_continue_unicode = internal_continue_unicode
+
+
+    @property
+    def internal_end_unicode(self):
+        return self.internal_end_unicode
+
+    @internal_end_unicode.setter
+    def internal_end_unicode(self, internal_end_unicode):
+        self.internal_end_unicode = internal_end_unicode
+
+
+    '''
+    Audio Unicode
+    '''
+    @property
+    def audio_begin_unicode(self):
+        return self.audio_begin_unicode
+
+    @audio_begin_unicode.setter
+    def audio_begin_unicode(self, audio_begin_unicode):
+        self.audio_begin_unicode = audio_begin_unicode
+
+    @property
+    def audio_continue_unicode(self):
+        return self.audio_continue_unicode
+
+    @audio_continue_unicode.setter
+    def audio_continue_unicode(self, audio_continue_unicode):
+        self.audio_continue_unicode = audio_continue_unicode
+
+
+    @property
+    def audio_end_unicode(self):
+        return self.audio_end_unicode
+
+    @audio_end_unicode.setter
+    def audio_end_unicode(self, audio_end_unicode):
+        self.audio_end_unicode = audio_end_unicode
 
 
 
 
 
+    '''
+    Telepathic character
+    '''
+    @property
+    def telepathic_begin_character(self):
+        return self.telepathic_begin_character
 
-'''
+    @telepathic_begin_character.setter
+    def telepathic_begin_character(self, telepathic_begin_character):
+        self.telepathic_begin_character = telepathic_begin_character
 
-          "dialogState": {
 
-            "dialogMark": [
-              "(,,[A-Za-z].*)|(,,\\.+[A-Za-z].*)|([A-Za-z].*\\s,,[A-Za-z].*)",
-              "(,,[A-Za-z].*\u0027\u0027)|(,,\\.+[A-Za-z].*\u0027\u0027)|([A-Za-z].*\\s,,[A-Za-z].*\u0027\u0027)|(\u0027\u0027[A-Za-z].*\u0027\u0027)|(\u0027\u0027\\.+[A-Za-z].*\u0027\u0027)|([A-Za-z].*\\s\u0027\u0027[A-Za-z].*\u0027\u0027)|([A-Za-z].*,\u0027\u0027 [A-Za-z].*)|([A-Za-z].*\u0027\u0027)",
-              "(,,[A-Za-z].*)|(,,\\.+[A-Za-z].*)|([A-Za-z].*\\s,,[A-Za-z].*)",
-              "(,,[A-Za-z].*\u0027\u0027)|(,,\\.+[A-Za-z].*\u0027\u0027)|([A-Za-z].*\\s,,[A-Za-z].*\u0027\u0027)|(\u0027\u0027[A-Za-z].*\u0027\u0027)|(\u0027\u0027\\.+[A-Za-z].*\u0027\u0027)|([A-Za-z].*\\s\u0027\u0027[A-Za-z].*\u0027\u0027)|([A-Za-z].*,\u0027\u0027 [A-Za-z].*)|([A-Za-z].*\u0027\u0027)"
-            ],
-           "telepathicBeginUnicode": "u003E",
-            "telepathicContinueUnicode": "u003Eu007C",
-            "telepathicEndUnicode": "u003C",
-            "internalBeginUnicode": "u003C",
-            "internalContinueUnicode": "u003Cu007C",
-            "internalEndUnicode": "u003E",
-            "audioBeginUnicode": "u002Cu002C",
-            "audioContinueUnicode": "u0027u0027",
-            "audioEndUnicode": "u0027u0027",
-            "telepathicBeginMark": "\u003e",
-            "telepathicContinueMark": "\u003e|",
-            "telepathicEndMark": "\u003c",
-            "internalBeginMark": "\u003c",
-            "internalContinueMark": "\u003c|",
-            "internalEndMark": "\u003e",
-            "audioBeginMark": ",,",
-            "audioContinueMark": "\u0027\u0027",
-            "audioEndMark": "\u0027\u0027",
-            "dialog": true,
-            "dialogBegin": true,
-            "dialogEnd": true,
-            "dialogContinue": false
-          },
-'''
+    @property
+    def telepathic_continue_character(self):
+        return self.telepathic_continue_character
+
+    @telepathic_continue_character.setter
+    def telepathic_continue_character(self, telepathic_continue_character):
+        self.telepathic_continue_character = telepathic_continue_character
+
+
+    @property
+    def telepathic_end_character(self):
+        return self.telepathic_end_character
+
+    @telepathic_end_character.setter
+    def telepathic_end_character(self, telepathic_end_character):
+        self.telepathic_end_character = telepathic_end_character
+
+
+    '''
+    Internal character
+    '''
+    @property
+    def internal_begin_character(self):
+        return self.internal_begin_character
+
+    @internal_begin_character.setter
+    def internal_begin_character(self, internal_begin_character):
+        self.internal_begin_character = internal_begin_character
+
+
+    @property
+    def internal_continue_character(self):
+        return self.internal_continue_character
+
+    @internal_continue_character.setter
+    def internal_continue_character(self, internal_continue_character):
+        self.internal_continue_character = internal_continue_character
+
+
+    @property
+    def internal_end_character(self):
+        return self.internal_end_character
+
+    @internal_end_character.setter
+    def internal_end_character(self, internal_end_character):
+        self.internal_end_character = internal_end_character
+
+
+    '''
+    Audio character
+    '''
+    @property
+    def audio_begin_character(self):
+        return self.audio_begin_character
+
+    @audio_begin_character.setter
+    def audio_begin_character(self, audio_begin_character):
+        self.audio_begin_character = audio_begin_character
+
+    @property
+    def audio_continue_character(self):
+        return self.audio_continue_character
+
+    @audio_continue_character.setter
+    def audio_continue_character(self, audio_continue_character):
+        self.audio_continue_character = audio_continue_character
+
+
+    @property
+    def audio_end_character(self):
+        return self.audio_end_character
+
+    @audio_end_character.setter
+    def audio_end_character(self, audio_end_character):
+        self.audio_end_character = audio_end_character
--- wordsum/read/models/dialog/dialog_meta.py
+++ wordsum/read/models/dialog/dialog_meta.py
@@ -1,7 +1,47 @@
-class Dialog_Meta(models.Model):
-    dialog = models.BooleanField()
-    dialog_being = models.BooleanField()
-    dialog_continue = models.BooleanField()
-    dialog_end  = models.BooleanField()
+class Dialog_Meta():
+
+
+    def __init__(self):
+        self.dialog = False
+        self.dialog_being = False
+        self.dialog_continue = False
+        self.dialog_end  = False
+
+
+    @property
+    def dialog(self):
+        return self.dialog
+
+    @dialog.setter
+    def dialog(self, dialog):
+        self.dialog = dialog
+
+
+    @property
+    def dialog_begin(self):
+        return self.dialog_begin
+
+    @dialog_begin.setter
+    def dialog_begin(self, dialog_begin):
+        self.dialog_begin = dialog_begin
+
+
+    @property
+    def dialog_continue(self):
+        return self.dialog_continue
+
+    @dialog_continue.setter
+    def dialog_continue(self, dialog_continue):
+        self.dialog_continue = dialog_continue
+
+
+    @property
+    def dialog_end(self):
+        return self.dialog_end
+
+    @dialog_end.setter
+    def dialog_end(self, dialog_end):
+        self.dialog_end = dialog_end
+
 
 
